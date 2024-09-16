a3dfd0c6148612f867125fbcd07af2a93fcad8ca
Jack Anderson <whotfisjax@gmail.com>
2024-09-16 18:12:14 -0400

    v0

--- src/App.js
+++ src/App.js
@@ -12,7 +12,14 @@ export default function Component() {
   useEffect(() => {
     fetch('/api/comments')
       .then(res => res.json())
-      .then(data => setReplies(data))
+      .then(data => {
+        // Adjust the timestamps to the user's local time
+        const adjustedReplies = data.map(reply => ({
+          ...reply,
+          timestamp: new Date(reply.timestamp).toLocaleString(),
+        }));
+        setReplies(adjustedReplies);
+      })
       .catch(err => console.error('Error fetching comments:', err));
   }, []);
 
@@ -37,6 +44,8 @@ export default function Component() {
       comment: comment.trim(),
       isSpoiler,
       timestamp: new Date().toLocaleString(),
+      // Simulate a new ID for demonstration purposes
+      _id: new Date().getTime().toString(),
     };
 
     fetch('/api/comments', {
@@ -46,7 +55,11 @@ export default function Component() {
     })
       .then((res) => res.json())
       .then((data) => {
-        setReplies([...replies, data]);
+        const adjustedData = {
+          ...data,
+          timestamp: new Date(data.timestamp).toLocaleString(),
+        };
+        setReplies([...replies, adjustedData]);
         setName('');
         setComment('');
         setIsSpoiler(false);
@@ -56,158 +69,161 @@ export default function Component() {
   };
 
   return (
-    <><div className="bg-[#EEF2FF] min-h-screen font-sans text-[13px] text-[#000000]">
-      {/* Header */}
-      <header className="bg-[#D1D5EE] border-b border-[#B7C5D9] p-1">
-        <div className="max-w-5xl mx-auto flex justify-between items-center">
-          <div className="flex space-x-4">
-            <a href="#" className="text-[#34345C] font-bold">[Twitter/X]</a>
-            <a href="#" className="text-[#34345C] font-bold">[Telegram]</a>
-            <a href="#" className="text-[#34345C] font-bold">[Pump]</a>
-          </div>
-          <div className="flex space-x-4">
-            <a href="#" className="text-[#34345C] font-bold">[a]</a>
-            <a href="#" className="text-[#34345C] font-bold">[b]</a>
-            <a href="#" className="text-[#34345C] font-bold">[c]</a>
-            <a href="#" className="text-[#34345C] font-bold hidden md:block">[d]</a>
-            <a href="#" className="text-[#34345C] font-bold hidden md:block">[e]</a>
-            <a href="#" className="text-[#34345C] font-bold hidden md:block">[f]</a>
-            <a href="#" className="text-[#34345C] font-bold hidden md:block">[g]</a>
-          </div>
-        </div>
-      </header>
-
-      <div className="max-w-5xl mx-auto p-2">
-        <div className="bg-[#D6DAF0] p-2 mb-4">
-          <h1 className="text-[#AF0A0F] font-bold text-lg">/truth/ - Conspiracy</h1>
-          <p className="text-xs mt-1">CA: processing...</p>
-        </div>
-
-        {/* Thread */}
-        <div className="bg-[#D6DAF0] p-2 mb-4">
-          {/* Original Post */}
-          <div className="p-2 bg-[#F0E0D6]">
-            <div className="flex items-start space-x-2">
-              <img
-                src="/truth.png"
-                alt="Post image"
-                className="w-[100px] md:w-[150px] border border-[#D9BFB7]" />
-              <div>
-                <div>
-                  <span className="font-bold text-[#117743]">Anonymous</span>
-                  <span className="text-xs"> 06/15/23(Thu)12:34:56 No.123456789</span>
-                </div>
-                <div className="text-xs mt-1">File: <span className="text-[#0000EE]">truth.jpg</span> (39 KB, 250x250)</div>
-                <p className="mt-2 whitespace-pre-wrap">
-                  Hey /truth/,
-
-                  after Trump getting almost eliminated for the 2nd time I started to think...wtf is really going on?
-                  What do you think? From Epstein to COVID to now. The last couple years have felt like a real fuckery, pardon my french.
-                  I'm putting together a team to dive in to every conspiracy to ever live and find out the truth. Care to join?
-                </p>
-              </div>
+    <>
+      <div className="bg-[#EEF2FF] min-h-screen font-sans text-[13px] text-[#000000]">
+        {/* Header */}
+        <header className="bg-[#D1D5EE] border-b border-[#B7C5D9] p-1">
+          <div className="max-w-5xl mx-auto flex justify-between items-center">
+            <div className="flex space-x-4">
+              <a href="#" className="text-[#34345C] font-bold">[Twitter/X]</a>
+              <a href="#" className="text-[#34345C] font-bold">[Telegram]</a>
+              <a href="#" className="text-[#34345C] font-bold">[Pump]</a>
+            </div>
+            <div className="flex space-x-4">
+              <a href="#" className="text-[#34345C] font-bold">[a]</a>
+              <a href="#" className="text-[#34345C] font-bold">[b]</a>
+              <a href="#" className="text-[#34345C] font-bold">[c]</a>
+              <a href="#" className="text-[#34345C] font-bold hidden md:block">[d]</a>
+              <a href="#" className="text-[#34345C] font-bold hidden md:block">[e]</a>
+              <a href="#" className="text-[#34345C] font-bold hidden md:block">[f]</a>
+              <a href="#" className="text-[#34345C] font-bold hidden md:block">[g]</a>
             </div>
           </div>
+        </header>
 
-          {/* Replies */}
-          <div className="mt-2 space-y-2">
-            <div className="p-2 bg-[#F0E0D6]">
-              <div>
-                <span className="font-bold text-[#117743]">Anonymous</span>
-                <span className="text-xs"> 06/15/23(Thu)12:35:23 No.123456790</span>
-              </div>
-              <p className="mt-2 whitespace-pre-wrap">
-                I'm in. My wife just can't know
-              </p>
-            </div>
+        <div className="max-w-5xl mx-auto p-2">
+          <div className="bg-[#D6DAF0] p-2 mb-4">
+            <h1 className="text-[#AF0A0F] font-bold text-lg">/truth/ - Conspiracy</h1>
+            <p className="text-xs mt-1">CA: processing...</p>
+          </div>
 
+          {/* Thread */}
+          <div className="bg-[#D6DAF0] p-2 mb-4">
+            {/* Original Post */}
             <div className="p-2 bg-[#F0E0D6]">
               <div className="flex items-start space-x-2">
                 <img
-                  src="/chem.png"
-                  alt="Reply image"
-                  className="w-[100px] h-[100px] border border-[#D9BFB7]" />
+                  src="/truth.png"
+                  alt="Post image"
+                  className="w-[100px] md:w-[150px] border border-[#D9BFB7]" />
                 <div>
                   <div>
                     <span className="font-bold text-[#117743]">Anonymous</span>
-                    <span className="text-xs"> 06/15/23(Thu)12:36:45 No.123456791</span>
+                    <span className="text-xs"> 06/15/23(Thu)12:34:56 No.123456789</span>
                   </div>
-                  <div className="text-xs mt-1">File: <span className="text-[#0000EE]">chemtrails.png</span> (15 KB, 100x100)</div>
+                  <div className="text-xs mt-1">File: <span className="text-[#0000EE]">truth.jpg</span> (39 KB, 250x250)</div>
                   <p className="mt-2 whitespace-pre-wrap">
-                    If you want real answers, look into chemtrails. The government is spraying chemicals in the sky to control us.
+                    Hey /truth/,
 
-                    Wake up, sheeple
+                    after Trump getting almost eliminated for the 2nd time I started to think...wtf is really going on?
+                    What do you think? From Epstein to COVID to now. The last couple years have felt like a real fuckery, pardon my french.
+                    I'm putting together a team to dive in to every conspiracy to ever live and find out the truth. Care to join?
                   </p>
                 </div>
               </div>
             </div>
 
-            <div className="p-2 bg-[#F0E0D6]">
-              <div>
-                <span className="font-bold text-[#117743]">Anonymous</span>
-                <span className="text-xs"> 06/15/23(Thu)12:37:12 No.123456792</span>
-              </div>
-              <p className="mt-2 whitespace-pre-wrap">
-                Chemtrails are just the tip of the iceberg. Bill Gates and the cabal made my cousin gay from the mosquitoes
-              </p>
-            </div>
-
-            {/* New replies */}
-            {replies.map((reply) => (
-              <div key={reply._id} className="p-2 bg-[#F0E0D6]">
+            {/* Replies */}
+            <div className="mt-2 space-y-2">
+              <div className="p-2 bg-[#F0E0D6]">
                 <div>
-                  <span className="font-bold text-[#117743]">{reply.name}</span>
-                  <span className="text-xs"> {reply.timestamp} No.{reply._id}</span>
+                  <span className="font-bold text-[#117743]">Anonymous</span>
+                  <span className="text-xs"> 06/15/23(Thu)12:35:23 No.123456790</span>
                 </div>
-                <p className={`mt-2 whitespace-pre-wrap ${reply.isSpoiler ? 'bg-black text-black hover:bg-transparent hover:text-inherit' : ''}`}>
-                  {reply.comment}
+                <p className="mt-2 whitespace-pre-wrap">
+                  I'm in. My wife just can't know
                 </p>
               </div>
-            ))}
+
+              <div className="p-2 bg-[#F0E0D6]">
+                <div className="flex items-start space-x-2">
+                  <img
+                    src="/chem.png"
+                    alt="Reply image"
+                    className="w-[100px] h-[100px] border border-[#D9BFB7]" />
+                  <div>
+                    <div>
+                      <span className="font-bold text-[#117743]">Anonymous</span>
+                      <span className="text-xs"> 06/15/23(Thu)12:36:45 No.123456791</span>
+                    </div>
+                    <div className="text-xs mt-1">File: <span className="text-[#0000EE]">chemtrails.png</span> (15 KB, 100x100)</div>
+                    <p className="mt-2 whitespace-pre-wrap">
+                      If you want real answers, look into chemtrails. The government is spraying chemicals in the sky to control us.
+
+                      Wake up, sheeple
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="p-2 bg-[#F0E0D6]">
+                <div>
+                  <span className="font-bold text-[#117743]">Anonymous</span>
+                  <span className="text-xs"> 06/15/23(Thu)12:37:12 No.123456792</span>
+                </div>
+                <p className="mt-2 whitespace-pre-wrap">
+                  Chemtrails are just the tip of the iceberg. Bill Gates and the cabal made my cousin gay from the mosquitoes
+                </p>
+              </div>
+
+              {/* New replies */}
+              {replies.map((reply) => (
+                <div key={reply._id} className="p-2 bg-[#F0E0D6]">
+                  <div>
+                    <span className="font-bold text-[#117743]">{reply.name}</span>
+                    <span className="text-xs"> {reply.timestamp} No.{reply._id}</span>
+                  </div>
+                  <p className={`mt-2 whitespace-pre-wrap ${reply.isSpoiler ? 'bg-black text-black hover:bg-transparent hover:text-inherit' : ''}`}>
+                    {reply.comment}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          {/* Reply Form */}
+          <div className="bg-[#D6DAF0] p-2">
+            <h3 className="font-bold mb-2 text-[#0F0C5D]">Reply to Thread No.123456789</h3>
+            {error && <p className="text-red-500 mb-2">{error}</p>}
+            <form className="space-y-2" onSubmit={handleFormSubmit}>
+              <div className="flex space-x-2">
+                <input
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  placeholder="Name (optional)"
+                  className="w-1/3 bg-[#F0E0D6] border-[#B7C5D9] p-1" />
+              </div>
+              <textarea
+                value={comment}
+                onChange={(e) => setComment(e.target.value)}
+                placeholder="Comment (required)"
+                className="w-full h-24 bg-[#F0E0D6] border-[#B7C5D9] p-1"
+                required />
+              <div className="flex items-center space-x-2">
+                <button
+                  type="submit"
+                  className="bg-[#0F0C5D] text-white p-1 hover:bg-[#0A0A3F] disabled:bg-gray-400"
+                  disabled={!comment.trim()}
+                >
+                  Post
+                </button>
+              </div>
+              <div className="flex items-center space-x-2">
+                <input
+                  type="checkbox"
+                  id="spoiler"
+                  checked={isSpoiler}
+                  onChange={(e) => setIsSpoiler(e.target.checked)} />
+                <label htmlFor="spoiler" className="text-[#34345C]">Spoiler</label>
+              </div>
+            </form>
           </div>
         </div>
-
-        {/* Reply Form */}
-        <div className="bg-[#D6DAF0] p-2">
-          <h3 className="font-bold mb-2 text-[#0F0C5D]">Reply to Thread No.123456789</h3>
-          {error && <p className="text-red-500 mb-2">{error}</p>}
-          <form className="space-y-2" onSubmit={handleFormSubmit}>
-            <div className="flex space-x-2">
-              <input
-                type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                placeholder="Name (optional)"
-                className="w-1/3 bg-[#F0E0D6] border-[#B7C5D9] p-1" />
-            </div>
-            <textarea
-              value={comment}
-              onChange={(e) => setComment(e.target.value)}
-              placeholder="Comment (required)"
-              className="w-full h-24 bg-[#F0E0D6] border-[#B7C5D9] p-1"
-              required />
-            <div className="flex items-center space-x-2">
-              <button
-                type="submit"
-                className="bg-[#0F0C5D] text-white p-1 hover:bg-[#0A0A3F] disabled:bg-gray-400"
-                disabled={!comment.trim()}
-              >
-                Post
-              </button>
-            </div>
-            <div className="flex items-center space-x-2">
-              <input
-                type="checkbox"
-                id="spoiler"
-                checked={isSpoiler}
-                onChange={(e) => setIsSpoiler(e.target.checked)} />
-              <label htmlFor="spoiler" className="text-[#34345C]">Spoiler</label>
-            </div>
-          </form>
-        </div>
       </div>
-    </div><div className='w-full flex justify-center py-2 bg-[#D6DAF0] text-xs px-[5%]'>
+      <div className='w-full flex justify-center py-2 bg-[#D6DAF0] text-xs px-[5%]'>
         <div>All trademarks and copyrights on this page are owned by their respective parties. Images uploaded are the responsibility of the Poster. Comments are owned by the Poster.</div>
-      </div></>
+      </div>
+    </>
   );
 }
