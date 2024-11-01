10fe20aa798ccff028209846cb3eb976770ec3ba
agorapilled <johngardner13@outlook.com>
2024-11-01 11:12:03 -0400

    v0

--- src/App.js
+++ src/App.js
@@ -79,9 +79,9 @@ export default function Component() {
         <header className="bg-[#D1D5EE] border-b border-[#B7C5D9] p-1">
           <div className="max-w-5xl mx-auto flex justify-between items-center">
             <div className="flex space-x-4">
-              <a href="https://x.com/findtruthsol" className="text-[#34345C] font-bold">[Twitter/X]</a>
-              <a href="https://t.me/TruthPortalsol" className="text-[#34345C] font-bold">[Telegram]</a>
-              <a href="https://pump.fun/CmesUZPFTjVNZmJsUTbShRCh2vSWtiKXVvxCmutgpump" className="text-[#34345C] font-bold">[Pump]</a>
+              <a href="https://x.com/" className="text-[#34345C] font-bold">[Twitter/X]</a>
+              <a href="https://t.me/" className="text-[#34345C] font-bold">[Telegram]</a>
+              <a href="https://pump.fun/board" className="text-[#34345C] font-bold">[Pump]</a>
             </div>
             <div className="flex space-x-4">
               <a href="#" className="text-[#34345C] font-bold">[a]</a>
@@ -97,8 +97,8 @@ export default function Component() {
 
         <div className="max-w-5xl mx-auto p-2">
           <div className="bg-[#D6DAF0] p-2 mb-4">
-            <h1 className="text-[#AF0A0F] font-bold text-lg">/truth/ - Conspiracy</h1>
-            <p className="text-xs mt-1">CA: CmesUZPFTjVNZmJsUTbShRCh2vSWtiKXVvxCmutgpump</p>
+            <h1 className="text-[#AF0A0F] font-bold text-lg">/p/ - puppet</h1>
+            <p className="text-xs mt-1">CA: updating</p>
           </div>
 
           {/* Thread */}
@@ -107,7 +107,7 @@ export default function Component() {
             <div className="p-2 bg-[#F0E0D6]">
               <div className="flex items-start space-x-2">
                 <img
-                  src="/truth.png"
+                  src="/pup.png"
                   alt="Post image"
                   className="w-[100px] md:w-[150px] border border-[#D9BFB7]" />
                 <div>
@@ -115,13 +115,11 @@ export default function Component() {
                     <span className="font-bold text-[#117743]">Anonymous</span>
                     <span className="text-xs"> 09/16/24(Mon)3:34:56 No.123456789</span>
                   </div>
-                  <div className="text-xs mt-1">File: <span className="text-[#0000EE]">truth.jpg</span> (39 KB, 250x250)</div>
+                  <div className="text-xs mt-1">File: <span className="text-[#0000EE]">puppet.jpg</span> (39 KB, 250x250)</div>
                   <p className="mt-2 whitespace-pre-wrap">
-                    Hey /truth/,
+                    Hey /p/,
 
-                    after Trump getting almost eliminated for the 2nd time I started to think...wtf is really going on?
-                    What do you think? From Epstein to COVID to now. The last couple years have felt like a real fuckery, pardon my french.
-                    I'm putting together a team to dive in to every conspiracy to ever live and find out the truth. Care to join?
+                    It's near election time and I'm worried that if Kamala wins then the shadowy cabal will be in charge. It doesn't seem like she is her own person. It's like she's a puppet but we don't know the true master. Maybe Israel? Bill Gates? Wyt? Anyways this year i'll be voting for Trump even though he may not be perfect. I don't want the lizard people in charge!
                   </p>
                 </div>
               </div>
@@ -135,14 +133,14 @@ export default function Component() {
                   <span className="text-xs"> 09/16/24(Mon)3:35:03 No.123456790</span>
                 </div>
                 <p className="mt-2 whitespace-pre-wrap">
-                  I'm in. My wife just can't know
+                  decode this: 01001011 01100001 01101101 01100001 01101100 01100001 00100000 01110111 01101111 01110010 01101011 01110011 00100000 01100110 01101111 01110010 00100000 01110100 01101000 01100101 00100000 01001010 01100101 01110111 01110011 00101110 00100000 01010111 01101111 01110010 01101100 01100100 00100000 01100100 01101111 01101101 01101001 01101110 01100001 01110100 01101001 01101111 01101110 00100000 01101001 01110011 00100000 01101001 01101101 01101101 01101001 01101110 01100101 01101110 01110100
                 </p>
               </div>
 
               <div className="p-2 bg-[#F0E0D6]">
                 <div className="flex items-start space-x-2">
                   <img
-                    src="/chem.png"
+                    src="/bill.jpg"
                     alt="Reply image"
                     className="w-[100px] h-[100px] border border-[#D9BFB7]" />
                   <div>
@@ -150,9 +148,9 @@ export default function Component() {
                       <span className="font-bold text-[#117743]">Anonymous</span>
                       <span className="text-xs"> 09/16/24(Mon)3:35:56 No.123456791</span>
                     </div>
-                    <div className="text-xs mt-1">File: <span className="text-[#0000EE]">chemtrails.png</span> (15 KB, 100x100)</div>
+                    <div className="text-xs mt-1">File: <span className="text-[#0000EE]">lizard.jpg</span> (15 KB, 100x100)</div>
                     <p className="mt-2 whitespace-pre-wrap">
-                      If you want real answers, look into chemtrails. The government is spraying chemicals in the sky to control us.
+                      If you want real answers, look into lizard people. Bill Gates is one to watch out for.
 
                       Wake up, sheeple
                     </p>
@@ -166,7 +164,7 @@ export default function Component() {
                   <span className="text-xs"> 09/16/24(Mon)3:35:56 No.123456792</span>
                 </div>
                 <p className="mt-2 whitespace-pre-wrap">
-                  Chemtrails are just the tip of the iceberg. Bill Gates and the cabal made my cousin gay from the mosquitoes
+                  Elections are just the tip of the iceberg. Bill Gates and the cabal made my cousin gay from the mosquitoes
                 </p>
               </div>
 
